129f641bc796e2252a7bfd51fde35ee663e6b621
JCGobbi <jcgobbi@yandex.com>
2023-04-17 13:43:09 -0300

    Rename Set_Preload_Enable to Set_Register_Preload, add Compute_Prescaler_And_Period and change Set_Compare_Value for HRTIM master, add HRTimer_Deadtime type and change Configure_Deadtime and add Set_Channel_Output_State in the HRTIM driver.

--- ADL/drivers/stm32h743/stm32-hrtimers.ads
+++ ADL/drivers/stm32h743/stm32-hrtimers.ads
@@ -68,9 +68,9 @@ package STM32.HRTimers is
    --  Stop all chosen timer counters at the same time if they don't have
    --  outputs enabled.
 
-   procedure Set_Preload_Enable
-     (This   : in out HRTimer_Master;
-      Enable : Boolean);
+   procedure Set_Register_Preload
+     (This    : in out HRTimer_Master;
+      Enabled : Boolean);
    --  Enables the registers preload mechanism and defines whether the write
    --  accesses to the memory mapped registers are done into HRTIM active or
    --  preload registers.
@@ -207,6 +207,16 @@ package STM32.HRTimers is
           Post => Current_Prescaler (This) = HRTimer_Prescaler_Value (Prescaler) and
                   Current_Period (This) = Period;
 
+   procedure Compute_Prescaler_And_Period
+     (This                : HRTimer_Master;
+      Requested_Frequency : UInt32;
+      Prescaler           : out HRTimer_Prescaler;
+      Period              : out UInt32)
+     with Pre => Requested_Frequency > 0;
+   --  Computes the minimum prescaler and thus the maximum resolution for the
+   --  given timer, based on the system clocks and the requested frequency.
+   --  Computes the period required for the requested frequency.
+
    type Counter_Operating_Mode is
      (SingleShot_NonRetriggerable,
       SingleShot_Retriggerable,
@@ -265,7 +275,7 @@ package STM32.HRTimers is
    procedure Set_Compare_Value
      (This    : in out HRTimer_Master;
       Compare : HRTimer_Compare_Number;
-      Value   : in out UInt16)
+      Value   : UInt16)
      with Post => Read_Compare_Value (This, Compare) = Value;
    --  Set the value for Compare registers 1 to 4.
 
@@ -531,6 +541,13 @@ package STM32.HRTimers is
 
    procedure Set_Period (This : in out HRTimer_Channel;  Value : UInt16)
      with Post => Current_Period (This) = Value;
+   --  The period values must be within a lower and an upper limit related to
+   --  the high-resolution implementation and listed in table 301 of the RM0433
+   --  rev 7 chapter 37.3.4.
+   --  Prescaler  CKPSC[2:0] Min      Max
+   --  Div_1      5          0x0003   0xFFFD
+   --  Div_2      6          0x0003   0xFFFD
+   --  Div_4      7          0x0003   0xFFFD
 
    function Current_Period (This : HRTimer_Channel) return UInt16;
 
@@ -680,7 +697,15 @@ package STM32.HRTimers is
       Compare : HRTimer_Compare_Number;
       Value   : UInt16)
      with Post => Current_Compare_Value (This, Compare) = Value;
-   --  Set the value for Compare registers 1 to 4.
+   --  Set the value for Compare registers 1 to 4. A compare value greater than
+   --  the period register value will not generate a compare match event.
+   --  The compare values must be within a lower and an upper limit related to
+   --  the high-resolution implementation and listed in table 301 of the RM0433
+   --  rev 7 chapter 37.3.4.
+   --  Prescaler  CKPSC[2:0] Min      Max
+   --  Div_1      5          0x0003   0xFFFD
+   --  Div_2      6          0x0003   0xFFFD
+   --  Div_4      7          0x0003   0xFFFD
 
    function Current_Compare_Value
      (This    : HRTimer_Channel;
@@ -841,18 +866,8 @@ package STM32.HRTimers is
       Source : HRTimer_Channel_DMA_Request) return Boolean;
 
    procedure Set_Deadtime (This : in out HRTimer_Channel; Enable : Boolean)
-     with Pre =>
-       (if This'Address = HRTIM_TIMA_Base then
-          not Enabled (This) or No_Outputs_Enabled (This)
-       elsif This'Address = HRTIM_TIMB_Base then
-          not Enabled (This) or No_Outputs_Enabled (This)
-       elsif This'Address = HRTIM_TIMC_Base then
-          not Enabled (This) or No_Outputs_Enabled (This)
-       elsif This'Address = HRTIM_TIMD_Base then
-          not Enabled (This) or No_Outputs_Enabled (This)
-       elsif This'Address = HRTIM_TIME_Base then
-          not Enabled (This) or No_Outputs_Enabled (This)),
-       Post => Enabled_Deadtime (This) = Enable;
+     with Pre  => not Enabled (This) or No_Outputs_Enabled (This),
+          Post => Enabled_Deadtime (This) = Enable;
    --  Enable or disable the deadtime. This parameter cannot be changed once
    --  the timer is operating (TxEN bit set) or if its outputs are enabled
    --  and set/reset by another timer. See RM0433 rev 7 Chapter 37.5.37 pg 1503.
@@ -860,11 +875,26 @@ package STM32.HRTimers is
    function Enabled_Deadtime (This : HRTimer_Channel) return Boolean;
    --  Return True if the timer deadtime is enabled.
 
+   type HRTimer_Deadtime_Prescaler is
+     (Mul_1,
+      Mul_2,
+      Mul_4,
+      Mul_8,
+      Mul_16,
+      Mul_32,
+      Mul_64,
+      Mul_128);
+   --  The Deadtime Prescaler input frequency is from HRTIM*8 (so the time is
+   --  tHRTIM/8) and determines the time for the Deadtime Generator (DTG).
+   --  tDTG is given by (2**(DTPRSC[2:0])) x (tHRTIM / 8).
+   --  The maximum Deadtime is given by tDTG output * 511 (UInt9) from the
+   --  rising and falling values.
+
    type HRTimer_Deadtime_Sign is (Positive_Sign, Negative_Sign);
 
    procedure Configure_Deadtime
      (This          : in out HRTimer_Channel;
-      Prescaler     : UInt3;
+      Prescaler     : HRTimer_Deadtime_Prescaler;
       Rising_Value  : UInt9;
       Rising_Sign   : HRTimer_Deadtime_Sign := Positive_Sign;
       Falling_Value : UInt9;
@@ -905,9 +935,15 @@ package STM32.HRTimers is
 
    function Read_Deadtime_Lock (This : HRTimer_Channel) return Deadtime_Lock;
 
+   type Output_State is (Low, High);
+
+   procedure Set_Channel_Output_State
+     (This  : in out HRTimer_Channel;
+      Out_1 : Output_State;
+      Out_2 : Output_State);
+
    type Output_Event is
-     (Software_Trigger,
-      Timer_A_Resynchronization,
+     (Timer_A_Resynchronization,
       Timer_Period,
       Timer_Compare_1,
       Timer_Compare_2,
@@ -940,6 +976,39 @@ package STM32.HRTimers is
       Register_Update);
    --  These events determine the set/reset crossbar of the outputs, so the
    --  output waveform is established.
+
+   for Output_Event use
+     (Timer_A_Resynchronization => 16#01#,
+      Timer_Period              => 16#02#,
+      Timer_Compare_1           => 16#03#,
+      Timer_Compare_2           => 16#04#,
+      Timer_Compare_3           => 16#05#,
+      Timer_Compare_4           => 16#06#,
+      Master_Period             => 16#07#,
+      Master_Compare_1          => 16#08#,
+      Master_Compare_2          => 16#09#,
+      Master_Compare_3          => 16#0A#,
+      Master_Compare_4          => 16#0B#,
+      Timer_Event_1             => 16#0C#,
+      Timer_Event_2             => 16#0D#,
+      Timer_Event_3             => 16#0E#,
+      Timer_Event_4             => 16#0F#,
+      Timer_Event_5             => 16#10#,
+      Timer_Event_6             => 16#11#,
+      Timer_Event_7             => 16#12#,
+      Timer_Event_8             => 16#13#,
+      Timer_Event_9             => 16#14#,
+      External_Event_1          => 16#15#,
+      External_Event_2          => 16#16#,
+      External_Event_3          => 16#17#,
+      External_Event_4          => 16#18#,
+      External_Event_5          => 16#19#,
+      External_Event_6          => 16#1A#,
+      External_Event_7          => 16#1B#,
+      External_Event_8          => 16#1C#,
+      External_Event_9          => 16#1D#,
+      External_Event_10         => 16#1E#,
+      Register_Update           => 16#1F#);
 
    type HRTimer_Channel_Output is (Output_1, Output_2);
 
@@ -1635,7 +1704,8 @@ package STM32.HRTimers is
       Periodic_Calibration : Boolean;
       Calibration_Rate     : DLL_Calibration);
    --  PLL calibration must be done before starting HRTIM master and timing
-   --  units. See RM0433 rev 7 Chapter 37.3.22 pg. 1450 for the sequence of
+   --  units. This routine exits only when the calibration is ended and HRTIM is
+   --  ready. See RM0433 rev 7 Chapter 37.3.22 pg. 1450 for the sequence of
    --  initialization.
 
    procedure Set_Fault_Input
